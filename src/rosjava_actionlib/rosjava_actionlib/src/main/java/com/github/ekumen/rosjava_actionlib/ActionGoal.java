323b6dd44b387fc2758ed0d12a479e77de765d28
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2015-11-19 13:58:50 -0300

    Added goal states.

--- src/rosjava_actionlib/rosjava_actionlib/src/main/java/com/github/ekumen/rosjava_actionlib/ActionGoal.java
+++ src/rosjava_actionlib/rosjava_actionlib/src/main/java/com/github/ekumen/rosjava_actionlib/ActionGoal.java
@@ -3,4 +3,22 @@ package com.github.ekumen.rosjava_actionlib;
 
 class ActionGoal
 {
+  public static class GoalStates
+  {
+    final static int PENDING = 0;  // The goal has yet to be processed by the action server
+    final static int ACTIVE = 1;  // The goal is currently being processed by the action server
+    final static int REJECTED = 2;  // The goal was rejected by the action server without being processed and without a request from the action client to cancel
+    final static int SUCCEEDED = 3;  // The goal was achieved successfully by the action server
+    final static int ABORTED = 4;  // The goal was aborted by the action server
+    final static int PREEMPTING = 5;  // Processing of the goal was canceled by either another goal, or a cancel request sent to the action server
+    final static int PREEMPTED = 6;  // The goal was preempted by either another goal, or a preempt message being sent to the action server
+    final static int RECALLING = 7;  // The goal has not been processed and a cancel request has been received from the action client, but the action server has not confirmed the goal is canceled
+    final static int RECALLED = 8; // The goal was canceled by either another goal, or a cancel request before the action server began processing the goal
+    final static int LOST = 9;  // The goal was sent
+  }
+
+  int findStatus(int statusArray)
+  {
+    return 0;
+  }
 }
