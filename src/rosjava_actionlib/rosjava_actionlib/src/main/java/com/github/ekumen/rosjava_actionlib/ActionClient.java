5f48a268075f02785e0769808ceea64aef7f3711
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2015-11-25 17:53:03 -0300

    Added skeleton for [ActionClient].

---
+++ src/rosjava_actionlib/rosjava_actionlib/src/main/java/com/github/ekumen/rosjava_actionlib/ActionClient.java
@@ -0,0 +1,18 @@
+
+public class ActionClient {
+  private void publishClient() {
+    
+  }
+
+  private void suscribeServer() {
+
+  }
+
+/**
+ * Publishes the client's topics and suscribes to the server's topics.
+ */
+  public void connect() {
+    publishClient();
+    suscribeServer();
+  }
+}
