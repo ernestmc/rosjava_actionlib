607ecad35b89796f1a64f7334e1df377e1e0ef15
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-01-12 11:41:11 -0300

    Added test for a skipped state case.

--- src/rosjava_actionlib/rosjava_actionlib/src/test/java/TestClientStateMachine.java
+++ src/rosjava_actionlib/rosjava_actionlib/src/test/java/TestClientStateMachine.java
@@ -6,11 +6,11 @@ import actionlib_msgs.GoalStatus;
 
 public class TestClientStateMachine {
   @Test
-  public void test() {
+  public void test1() {
+    // test a full branch transition from goal ack to waiting for result
     int state;
     ClientStateMachine stateMachine = new ClientStateMachine();
 
-    //
     // set initial state
     stateMachine.setState(ClientStateMachine.ClientStates.WAITING_FOR_GOAL_ACK);
     // transition to next states
@@ -19,6 +19,19 @@ public class TestClientStateMachine {
     stateMachine.transition(actionlib_msgs.GoalStatus.PREEMPTING);
     assertEquals(ClientStateMachine.ClientStates.PREEMPTING, stateMachine.getState());
     stateMachine.transition(actionlib_msgs.GoalStatus.SUCCEEDED);
+    assertEquals(ClientStateMachine.ClientStates.WAITING_FOR_RESULT, stateMachine.getState());
+  }
+
+  @Test
+  public void test2() {
+    // test a vector of states transition for a skipped test
+    int state;
+    ClientStateMachine stateMachine = new ClientStateMachine();
+
+    // set initial state
+    stateMachine.setState(ClientStateMachine.ClientStates.WAITING_FOR_CANCEL_ACK);
+    // transition to next states
+    stateMachine.transition(actionlib_msgs.GoalStatus.RECALLED);
     assertEquals(ClientStateMachine.ClientStates.WAITING_FOR_RESULT, stateMachine.getState());
   }
 }
